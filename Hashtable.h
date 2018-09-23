aa90f0f65ed99184a6db217ab9dd804d14ed17e7
unknown-2018 <43525087+unknown-2018@users.noreply.github.com>
2018-09-23 22:20:32 +0100

    Add files via upload

---
+++ Hashtable.h
@@ -0,0 +1,19 @@
+/*
+
+- Hash table class used in the U-PSI protocol.
+
+*/
+
+
+#include"Rand.h"
+
+//**********************************************************************
+
+class Hashtable{
+
+public:
+	Hashtable ( int elem_in_bucket, bigint* elemen, int elem_size,int table_size);
+	bigint* get_bucket(int index);
+private:
+	bigint **T;// hash table
+};
