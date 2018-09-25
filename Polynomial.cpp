b22696dbde28b9f6032e27733fdcaf1e12ce2fe5
unknown-2018 <43525087+unknown-2018@users.noreply.github.com>
2018-09-25 18:11:13 +0100

    Update Polynomial.cpp

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -7,7 +7,7 @@
 
 #include"Polynomial.h"
 //**********************************************************************
-// - Description: Constrcutor- given an array of elements and x-coordinates constructs a polynomial in the point-value form, i.e. calculates y-coordinates.
+// - Description: Constructor- given an array of elements and x-coordinates constructs a polynomial in the point-value form, i.e. calculates y-coordinates.
 
 Polynomial::Polynomial(bigint* elem, string ID, bigint * xpoints, int elem_size, int xpoints_size, bigint pubmoduli){
 
